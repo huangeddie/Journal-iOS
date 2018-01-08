d39d131c62dc2e4d01ae22d9477abe2080ce4cd7
Eddie Huang <aigagror@gmail.com>
2018-01-08 11:57:27 -0500

    First commit

---
+++ Journal/NewEntryViewController.swift
@@ -0,0 +1,44 @@
+//
+//  NewEntryViewController.swift
+//  Journal
+//
+//  Created by Edward Huang on 1/7/18.
+//  Copyright © 2018 Eddie Huang. All rights reserved.
+//
+
+import UIKit
+
+class NewEntryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func cancelPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+ 
+    @IBAction func titleChanged(_ sender: UITextField) {
+        navigationItem.title = sender.text
+    }
+    
+    
+}
