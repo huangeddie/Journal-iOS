ddcb0f2bc4945c9dc83b21bfb77df4b033fc3fb8
Eddie Huang <aigagror@gmail.com>
2018-01-23 20:38:03 -0600

    stuff

---
+++ Journal/Views/JournalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  JournalTableViewCell.swift
+//  Journal
+//
+//  Created by Edward Huang on 1/22/18.
+//  Copyright © 2018 Eddie Huang. All rights reserved.
+//
+
+import UIKit
+
+class JournalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
